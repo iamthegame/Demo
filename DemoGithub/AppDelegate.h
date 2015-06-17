5532849259a889c9277f85c5d2c9db10e054d0ea
Kuldeep Saini <kulsaini@in.ibm.com>
2015-06-17 11:39:31 +0530

    initial commit

---
+++ DemoGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoGithub
+//
+//  Created by Kuldeep Saini on 17/06/15.
+//  Copyright (c) 2015 Kuldeep Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoGithub
+//
+//  Created by Kuldeep Saini on 17/06/15.
+//  Copyright (c) 2015 Kuldeep Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
